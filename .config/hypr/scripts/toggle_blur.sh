1389109f888f178b7f97494664f582cc71d38fb1
jacobeulogio <benitezjacob35@gmail.com>
2025-06-19 04:06:39 +0800

    Updates

--- .config/hypr/scripts/toggle_blur.sh
+++ .config/hypr/scripts/toggle_blur.sh
@@ -5,9 +5,10 @@ if [ "$HYPR_BLUR" -gt 0 ] ; then
     hyprctl --batch "\
         keyword decoration:blur:enabled 0;\
         keyword decoration:inactive_opacity 1"
+    notify-send "Hyprland" "Disabled blur and Opacity"
     exit
 fi
 
 hyprctl reload
-
+notify-send "Hyprland" "Reverted to default config"
 
