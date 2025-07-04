62508d484912250ec3f68747f280ea1a5f3dedd2
jacobeulogio <benitezjacob35@gmail.com>
2025-07-04 21:02:02 +0800

    Changed hyperland toggle_gaps script

--- .config/hypr/scripts/toggle_gaps.sh
+++ .config/hypr/scripts/toggle_gaps.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 HYPR_GAPS=$(hyprctl getoption general:gaps_out | awk '/custom type:/ { if ($3 > 0) print 1; else print 0 }')
-if [ "$HYPR_GAPS" -gt 0 ] ; then
+if [ "$HYPR_GAPS" -eq 0 ] ; then
     hyprctl --batch "\
         keyword decoration:shadow:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
+        keyword general:gaps_in 4;\
+        keyword general:gaps_out 8;\
         keyword general:border_size 2;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 10"
     exit
 fi
 
