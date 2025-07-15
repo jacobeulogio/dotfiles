354eafc93ae584d06479ea558f44a2f323fe6fad
jacobbenitez <jacob.benitez@triumphhomedepot.com>
2025-07-15 17:21:21 +0800

    updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ config.initial_rows = 46
 
 config.font_size = 13
 
-wezterm.font("BlexMono Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
+-- wezterm.font("BlexMono Nerd Font Mono")
 
 config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.colors = {
