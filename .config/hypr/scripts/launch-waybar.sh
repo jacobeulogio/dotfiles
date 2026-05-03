b78cc3f6789b8929245c8c126c84c7e9faecfaa4
jacobeulogio <benitezjacob35@gmail.com>
2026-05-03 21:08:04 +0800

    updates

--- .config/hypr/scripts/launch-waybar.sh
+++ .config/hypr/scripts/launch-waybar.sh
@@ -6,12 +6,13 @@ pkill waybar
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-# If an external monitor is connected (DP-2 or HDMI-A-2), use the docked config.
-# Otherwise, use the laptop config.
-if hyprctl monitors | grep -q -e "Xiaomi" -e "HDMI-A-2"; then
-    hyprctl keyword monitor eDP-2,preferred,auto,1
-    waybar -c ~/.config/waybar/config-docked.jsonc &
-else
-    hyprctl keyword monitor eDP-2,preferred,auto,1
-    waybar -c ~/.config/waybar/config-laptop.jsonc &
-fi
+waybar & disown
+# # If an external monitor is connected (DP-2 or HDMI-A-2), use the docked config.
+# # Otherwise, use the laptop config.
+# if hyprctl monitors | grep -q -e "Xiaomi" -e "HDMI-A-2"; then
+#     hyprctl keyword monitor eDP-2,preferred,auto,1
+#     waybar -c ~/.config/waybar/config-docked.jsonc &
+# else
+#     hyprctl keyword monitor eDP-2,preferred,auto,1
+#     waybar -c ~/.config/waybar/config-laptop.jsonc &
+# fi
